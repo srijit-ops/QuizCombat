711a6272ab3c152db674cbe0d6bb352071c2f6a7
srijit-ops <srijitasengupta23@gmail.com>
2024-04-28 19:59:14 +0530

    loader added

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 function Loader() {
   return (
-    <div className='text-white'>Loader</div>
-  )
+    <div className="flex justify-center items-center w-full h-screen">
+      <Player
+        src="https://lottie.host/83313cd9-7c35-42c7-8583-cf4c05a29566/2X2ul9NFam.json"
+        className="player"
+        loop
+        autoplay
+        style={{ height: "250px", width: "250px" }}
+      />
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
